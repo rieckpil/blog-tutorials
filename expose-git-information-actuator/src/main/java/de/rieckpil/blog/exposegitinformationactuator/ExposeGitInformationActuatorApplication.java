91c16c25b3e57b74de270ac5eac58fcf90c5b79a
Philip Riecks <philip@riecks.io>
2018-06-02 09:31:06 +0200

    initial commit

---
+++ expose-git-information-actuator/src/main/java/de/rieckpil/blog/exposegitinformationactuator/ExposeGitInformationActuatorApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.blog.exposegitinformationactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposeGitInformationActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExposeGitInformationActuatorApplication.class, args);
+    }
+}
