e8b0200f6d70a1ea42b71476190eaf85c26cd91b
Philip Riecks <philip@riecks.io>
2018-07-08 16:39:15 +0200

    add new code base for blog article about SendGrid

---
+++ send-emails-with-sendgrid-and-spring-boot/src/main/java/de/rieckpil/blog/sendemailswithsendgridandspringboot/MailController.java
@@ -0,0 +1,55 @@
+package de.rieckpil.blog.sendemailswithsendgridandspringboot;
+
+import com.sendgrid.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class MailController {
+
+    @Autowired
+    private SendGrid sendGrid;
+
+    @Value("${templateId}")
+    private String EMAIL_TEMPLATE_ID;
+
+    @GetMapping("/sendgrid")
+    public String sendEmailWithSendGrid(@RequestParam("msg") String message) {
+
+        Email from = new Email("yourname@yourhostname.de");
+        String subject = "Hello World!";
+        Email to = new Email("yourname@yourhostname.de");
+        Content content = new Content("text/html", "I'm replacing the <strong>body tag</strong>" + message);
+
+        Mail mail = new Mail(from, subject, to, content);
+
+        mail.setReplyTo(new Email("yourname@yourhostname.de"));
+        mail.personalization.get(0).addSubstitution("-username-", "Some blog user");
+        mail.setTemplateId(EMAIL_TEMPLATE_ID);
+
+        Request request = new Request();
+        Response response = null;
+
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+
+            response = sendGrid.api(request);
+
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return "email was successfully send";
+    }
+}
---
+++ send-emails-with-sendgrid-and-spring-boot/src/main/java/de/rieckpil/blog/sendemailswithsendgridandspringboot/SendEmailsWithSendgridAndSpringBootApplication.java
@@ -0,0 +1,12 @@
+package de.rieckpil.blog.sendemailswithsendgridandspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailsWithSendgridAndSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendEmailsWithSendgridAndSpringBootApplication.class, args);
+    }
+}
