cbe4da12d578409fd4139d2bce336754a95697e6
Philip Riecks <philip@riecks.io>
2018-08-13 18:00:48 +0200

    update jsf security blog post

--- jsf-simple-login-with-java-ee-security-api/src/main/java/de/rieckpil/blog/LoginBacking.java
+++ jsf-simple-login-with-java-ee-security-api/src/main/java/de/rieckpil/blog/LoginBacking.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.IOException;
 
