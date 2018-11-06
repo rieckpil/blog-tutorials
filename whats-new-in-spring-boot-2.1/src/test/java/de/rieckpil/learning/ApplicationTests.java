e1d02a53fbe54fe1b409739f878fde8b6ff52830
Philip Riecks <mail@philipriecks.de>
2018-11-06 15:03:21 +0100

    add JUnit 5 example

--- whats-new-in-spring-boot-2.1/src/test/java/de/rieckpil/learning/ApplicationTests.java
+++ whats-new-in-spring-boot-2.1/src/test/java/de/rieckpil/learning/ApplicationTests.java
@@ -1,15 +1,15 @@
 package de.rieckpil.learning;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+// no @ExtendWith(SpringExtension.class) needed
 public class ApplicationTests {
 
 	@Test
+	@DisplayName("Load the whole Spring context")
 	public void contextLoads() {
 	}
 
