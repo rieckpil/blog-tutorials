b9e576dd9d36610569d9ab7a22180d7d7d8d31a0
Philip Riecks <mail@philipriecks.de>
2018-12-19 07:43:01 +0100

    remove unused interface extensions of JPA repository

--- dynamic-sql-querying-with-pagination/src/main/java/de/rieckpil/tutorials/PersonRepository.java
+++ dynamic-sql-querying-with-pagination/src/main/java/de/rieckpil/tutorials/PersonRepository.java
@@ -1,11 +1,8 @@
 package de.rieckpil.tutorials;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
 
-public interface PersonRepository extends JpaRepository<Person, Long>, QueryByExampleExecutor<Person>,
-		JpaSpecificationExecutor<Person>, QuerydslPredicateExecutor<Person> {
+public interface PersonRepository extends JpaRepository<Person, Long>, QuerydslPredicateExecutor<Person> {
 
 }
