22c6eb092f81c88db7c5065335837fa69f4b1ba1
Philip Riecks <mail@philipriecks.de>
2019-02-24 16:23:25 +0100

    update deployment scripts for Open Liberty

--- charts-in-pdf-java-ee/buildAndRun.bat
+++ charts-in-pdf-java-ee/buildAndRun.bat
@@ -2,4 +2,4 @@
 call mvn clean package
 call docker build -t de.rieckpil.blog/charts-in-pdf-java-ee .
 call docker rm -f charts-in-pdf-java-ee
-call docker run -d -p 8080:8080 -p 4848:4848 --name charts-in-pdf-java-ee de.rieckpil.blog/charts-in-pdf-java-ee
+call docker run -d -p 9080:9080 --name charts-in-pdf-java-ee de.rieckpil.blog/charts-in-pdf-java-ee
