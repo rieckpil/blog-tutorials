dad6e207600b660c103de9bd1d3760c97b3117ef
Philip Riecks <mail@philipriecks.de>
2019-03-31 21:07:02 +0200

    add k8s detector

--- whats-new-in-spring-boot.2.2/src/main/java/de/rieckpil/blog/Application.java
+++ whats-new-in-spring-boot.2.2/src/main/java/de/rieckpil/blog/Application.java
@@ -1,13 +1,18 @@
 package de.rieckpil.blog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 	}
 
 }
