4a1c7d2ded7c5f19d6e8051f74bb790ce60b1da7
Philip Riecks <philip@riecks.io>
2019-04-05 16:29:25 +0200

    add missing classes

---
+++ spring-boot-hibernate-flyway-best-practices/src/main/java/de/rieckpil/blog/SpringBootHibernateFlywayBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package de.rieckpil.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateFlywayBestPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateFlywayBestPracticesApplication.class, args);
+	}
+
+}
