51ac912c79e28b24d1277d1a8ce1b4be2d17ba45
Philip Riecks <mail@philipriecks.de>
2019-06-01 13:11:18 +0200

    add new codebase for blog post about CRUD

---
+++ demo-crud-application/src/main/java/de/rieckpil/blog/BookInitializer.java
@@ -0,0 +1,38 @@
+package de.rieckpil.blog;
+
+import com.github.javafaker.Faker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class BookInitializer implements CommandLineRunner {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Faker faker = new Faker();
+
+        log.info("Starting book initialization ...");
+
+        for(int i = 0; i < 10; i++) {
+
+            Book book = new Book();
+            book.setTitle(faker.book().title());
+            book.setAuthor(faker.book().author());
+            book.setIsbn(UUID.randomUUID().toString());
+
+            bookRepository.save(book);
+        }
+
+        log.info("... finished book initialization");
+
+    }
+}
---
+++ demo-crud-application/src/main/java/de/rieckpil/blog/DemoCrudApplication.java
@@ -0,0 +1,13 @@
+package de.rieckpil.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCrudApplication.class, args);
+    }
+
+}
