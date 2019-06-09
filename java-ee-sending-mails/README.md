eb81c4890b320fe808e05956ef2a50d123ac94c9
rieckpil <mail@philipriecks.de>
2019-06-09 18:55:45 +0200

    finish code base for Java EE Mail API example

---
+++ java-ee-sending-mails/README.md
@@ -0,0 +1,11 @@
+# Codebase for the blog post [#HOWTO: Send emails with Java EE using Payara](https://rieckpil.de/howto-send-emails-with-java-ee-using-payara)
+
+Steps to run this project:
+
+1. Clone this Git repository
+2. Navigate to the folder `java-ee-sending-mails`
+3. Replace the targeted email in `src/main/resources/META-INF/microprofile-config.properties` with yours
+4. Build the project with `mvn clean package`
+5. Start your Docker daemon (`docker-compose` required)
+6. Run `docker-compuse build && docker-compose up`
+7. Visit `http://localhost:8080/resources/mails` to send a sample email to your address (the incoming email might be blocked by your email provider or marked as spam)
--- java-ee-sending-mails/src/main/java/de/rieckpil/blog/MailingService.java
+++ java-ee-sending-mails/src/main/java/de/rieckpil/blog/MailingService.java
@@ -2,6 +2,8 @@ package de.rieckpil.blog;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -10,7 +12,10 @@ import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
 
 @Stateless
 public class MailingService {
@@ -30,8 +35,23 @@ public class MailingService {
         try {
             simpleMail.setSubject("Hello World from Java EE!");
             simpleMail.setRecipient(Message.RecipientType.TO, new InternetAddress(emailAddress));
-            simpleMail.setContent("<p>Hello <b>World!</b></p>", "text/html; charset=utf-8");
+
+            MimeMultipart mailContent = new MimeMultipart();
+
+            MimeBodyPart mailMessage = new MimeBodyPart();
+            mailMessage.setContent("<p>Take a look at the <b>scecretMessage.txt</b> file</p>", "text/html; charset=utf-8");
+            mailContent.addBodyPart(mailMessage);
+
+            MimeBodyPart mailAttachment = new MimeBodyPart();
+            DataSource source = new ByteArrayDataSource("This is a secret message".getBytes(), "text/plain");
+            mailAttachment.setDataHandler(new DataHandler(source));
+            mailAttachment.setFileName("secretMessage.txt");
+
+            mailContent.addBodyPart(mailAttachment);
+            simpleMail.setContent(mailContent);
+
             Transport.send(simpleMail);
+
             System.out.println("Message successfully send to: " + emailAddress);
         } catch (MessagingException e) {
             e.printStackTrace();
