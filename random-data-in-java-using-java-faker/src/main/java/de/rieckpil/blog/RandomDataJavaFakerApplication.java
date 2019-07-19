38161e47ca20f454f01dab448b39cb3fc068d60a
Philip Riecks <mail@philipriecks.de>
2019-07-19 22:13:21 +0200

    add new project

---
+++ random-data-in-java-using-java-faker/src/main/java/de/rieckpil/blog/RandomDataJavaFakerApplication.java
@@ -0,0 +1,13 @@
+package de.rieckpil.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomDataJavaFakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomDataJavaFakerApplication.class, args);
+    }
+
+}
