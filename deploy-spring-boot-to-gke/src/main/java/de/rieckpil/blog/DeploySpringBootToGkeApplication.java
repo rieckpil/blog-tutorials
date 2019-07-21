6431947c8446ce2f33e8ae6b0d5fba0739480485
Philip Riecks <mail@philipriecks.de>
2019-07-21 13:09:17 +0200

    add new project

---
+++ deploy-spring-boot-to-gke/src/main/java/de/rieckpil/blog/DeploySpringBootToGkeApplication.java
@@ -0,0 +1,13 @@
+package de.rieckpil.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySpringBootToGkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploySpringBootToGkeApplication.class, args);
+    }
+
+}
---
+++ deploy-spring-boot-to-gke/src/main/java/de/rieckpil/blog/HelloWorldController.java
@@ -0,0 +1,20 @@
+package de.rieckpil.blog;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping
+    public ResponseEntity<String> getMessage() {
+        return ResponseEntity.ok(message);
+    }
+}
