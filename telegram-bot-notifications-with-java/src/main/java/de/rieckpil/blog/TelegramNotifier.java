55b96eeb22c6cf46d1a8c9a76b38164d18f5aca6
Philip Riecks <mail@philipriecks.de>
2019-08-02 07:09:42 +0200

    add new project for Telegram Bot

---
+++ telegram-bot-notifications-with-java/src/main/java/de/rieckpil/blog/TelegramNotifier.java
@@ -0,0 +1,41 @@
+package de.rieckpil.blog;
+
+import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class TelegramNotifier {
+
+    private static final String CHAT_ID = "INSERT_HERE";
+    private static final String TOKEN = "INSERT_HERE";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String message = "Hello World from Java 11";
+
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(5))
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+
+        UriBuilder builder = UriBuilder
+                .fromUri("https://api.telegram.org")
+                .path("/{token}/sendMessage")
+                .queryParam("chat_id", CHAT_ID)
+                .queryParam("text", message);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(builder.build("bot" + TOKEN))
+                .timeout(Duration.ofSeconds(5))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+    }
+}
