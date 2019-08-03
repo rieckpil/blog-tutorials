adc4f30011d840231c9fcf0018187dc192240a68
rieckpil <mail@philipriecks.de>
2019-08-03 15:31:07 +0200

    adjust try with resources

--- running-java-within-postgres/simple-java-function/src/main/java/de/rieckpil/blog/Functions.java
+++ running-java-within-postgres/simple-java-function/src/main/java/de/rieckpil/blog/Functions.java
@@ -3,6 +3,7 @@ package de.rieckpil.blog;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
@@ -14,51 +15,51 @@ import org.postgresql.pljava.annotation.Function;
 
 public class Functions {
 
-	@Function
-	public static String greet(String personName) {
-		return "Hello World, " + personName + " !";
-	}
+    @Function
+    public static String greet(String personName) {
+        return "Hello World, " + personName + " !";
+    }
 
-	@Function(name = "split_string_by_delimiter")
-	public static Iterator<String> splitStringByDelimiter(String tagString, String delimiter) {
+    @Function(name = "split_string_by_delimiter")
+    public static Iterator<String> splitStringByDelimiter(String tagString, String delimiter) {
 
-		if (delimiter == null || delimiter.isEmpty()) {
-			delimiter = ">";
-		}
+        if (delimiter == null || delimiter.isEmpty()) {
+            delimiter = ">";
+        }
 
-		List<String> tags = new ArrayList<String>();
+        List<String> tags = new ArrayList<String>();
 
-		for (String currentTag : tagString.split(delimiter)) {
-			tags.add(currentTag.trim());
-		}
+        for (String currentTag : tagString.split(delimiter)) {
+            tags.add(currentTag.trim());
+        }
 
-		return tags.iterator();
-	}
+        return tags.iterator();
+    }
 
-	@Function(name = "get_oldest_person")
-	public static String getOldestPerson() throws SQLException {
+    @Function(name = "get_oldest_person")
+    public static String getOldestPerson() throws SQLException {
 
-		try(ResultSet resultSet = DriverManager //
-				.getConnection("jdbc:default:connection") //
-				.createStatement() //
-				.executeQuery("SELECT * FROM persons")) {
+        try (Statement statement = DriverManager //
+                .getConnection("jdbc:default:connection") //
+                .createStatement(); //
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM persons")) {
 
-			List<Person> personList = new ArrayList<>();
+            List<Person> personList = new ArrayList<>();
 
-			while (resultSet.next()) {
-				Person person = new Person();
-				person.setId(resultSet.getLong("id"));
-				person.setFirstName(resultSet.getString("first_name"));
-				person.setLastName(resultSet.getString("last_name"));
-				person.setDayOfBirth(resultSet.getDate("day_of_birth").toLocalDate());
-				personList.add(person);
-			}
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getLong("id"));
+                person.setFirstName(resultSet.getString("first_name"));
+                person.setLastName(resultSet.getString("last_name"));
+                person.setDayOfBirth(resultSet.getDate("day_of_birth").toLocalDate());
+                personList.add(person);
+            }
 
-			Collections.sort(personList);
-			Person oldestPerson = personList.get(0);
+            Collections.sort(personList);
+            Person oldestPerson = personList.get(0);
 
-			return String.format("The oldest person is %s, %s with %s years!", oldestPerson.getFirstName(),
-					oldestPerson.getLastName(), Period.between(oldestPerson.getDayOfBirth(), LocalDate.now()).getYears());
-		}
-	}
+            return String.format("The oldest person is %s, %s with %s years!", oldestPerson.getFirstName(),
+                    oldestPerson.getLastName(), Period.between(oldestPerson.getDayOfBirth(), LocalDate.now()).getYears());
+        }
+    }
 }
