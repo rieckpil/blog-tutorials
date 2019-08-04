686f290fc3ae47cdb6531e9a51fde2e4662607d9
Philip Riecks <mail@philipriecks.de>
2019-08-04 18:42:30 +0200

    configure Kotlin project to run on OpenLiberty

--- java-ee-with-kotlin/buildAndRun.bat
+++ java-ee-with-kotlin/buildAndRun.bat
@@ -2,4 +2,4 @@
 call mvn clean package
 call docker build -t de.rieckpil.blog/java-ee-with-kotlin .
 call docker rm -f java-ee-with-kotlin
-call docker run -d -p 8080:8080 -p 4848:4848 --name java-ee-with-kotlin de.rieckpil.blog/java-ee-with-kotlin
+call docker run -d -p 9080:9080 --name java-ee-with-kotlin de.rieckpil.blog/java-ee-with-kotlin
--- java-ee-with-kotlin/buildAndRun.sh
+++ java-ee-with-kotlin/buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean package && docker build -t de.rieckpil.blog/java-ee-with-kotlin .
-docker rm -f java-ee-with-kotlin || true && docker run -d -p 8080:8080 -p 4848:4848 --name java-ee-with-kotlin de.rieckpil.blog/java-ee-with-kotlin
+docker rm -f java-ee-with-kotlin || true && docker run -d -p 9080:9080 --name java-ee-with-kotlin de.rieckpil.blog/java-ee-with-kotlin
