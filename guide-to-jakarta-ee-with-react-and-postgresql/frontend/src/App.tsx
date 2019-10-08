b5e0e4b1fcddea6921ad5ed3a831a37656bd797c
Philip Riecks <mail@philipriecks.de>
2019-10-08 06:32:36 +0200

    add basic react setup for new post

---
+++ guide-to-jakarta-ee-with-react-and-postgresql/frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Hello World!</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
