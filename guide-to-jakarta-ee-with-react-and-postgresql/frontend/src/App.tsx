78e7f4bce5df4f0fea0e5b8ba4de6b2662c1f2eb
Philip Riecks <mail@philipriecks.de>
2019-10-09 07:09:13 +0200

    add semantic ui to project

--- guide-to-jakarta-ee-with-react-and-postgresql/frontend/src/App.tsx
+++ guide-to-jakarta-ee-with-react-and-postgresql/frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'semantic-ui-react'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello World!</code>
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    <code>Hello World!</code>
+                </p>
+                <Button>Click me for more information!</Button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
