8e18f98010d2cc484f248dc0ccc0909fc7e6a909
Philip Riecks <philip.riecks@scout24.com>
2019-10-14 19:15:52 +0200

    update MP 3.1 examples

--- whats-new-in-microprofile-3.1/src/main/java/de/rieckpil/blog/HealthUpdates.java
+++ whats-new-in-microprofile-3.1/src/main/java/de/rieckpil/blog/HealthUpdates.java
@@ -1,4 +1,16 @@
 package de.rieckpil.blog;
 
-public class HealthUpdates {
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class HealthUpdates implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        // HealthCheckResponse.named("my-liveness-check").up().build();
+        // HealthCheckResponse.builder().name("my-liveness-check").up().build();
+        return HealthCheckResponse.up("my-liveness-check");
+    }
 }
--- whats-new-in-microprofile-3.1/src/main/java/de/rieckpil/blog/MetricsUpdates.java
+++ whats-new-in-microprofile-3.1/src/main/java/de/rieckpil/blog/MetricsUpdates.java
@@ -1,4 +1,29 @@
 package de.rieckpil.blog;
 
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.Metadata;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.eclipse.microprofile.metrics.MetricType;
+
+import javax.inject.Inject;
+
 public class MetricsUpdates {
+
+    @Inject
+    private MetricRegistry metricRegistry;
+
+    public void updates() {
+        Metadata metadata = Metadata
+                .builder()
+                .reusable(true)
+                .withName("myMetric")
+                .withUnit("seconds")
+                .withDescription("counting seconds")
+                .withType(MetricType.COUNTER)
+                .build();
+
+        Counter counterOne = metricRegistry.counter(metadata);
+        Counter counterTwo = metricRegistry.counter(metadata);
+
+    }
 }
