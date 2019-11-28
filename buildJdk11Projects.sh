fd60e555815a9127ccc810f25a6745bf86096597
Philip Riecks <mail@philipriecks.de>
2019-11-28 07:23:48 +0100

    add new project

--- buildJdk11Projects.sh
+++ buildJdk11Projects.sh
@@ -4,7 +4,8 @@ declare -a arr=("whats-new-in-spring-boot-2.1"
  "spring-boot-hibernate-flyway-best-practices"
  "random-data-in-java-using-java-faker"
  "java-ee-with-kotlin"
- "spring-boot-feature-toggles-with-togglz")
+ "spring-boot-feature-toggles-with-togglz"
+ "spring-boot-integration-tests-wiremock")
 
 for project in "${arr[@]}"
 do
