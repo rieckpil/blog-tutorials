d9c6050cf150a60260cb5acb0e5f0382e6200d32
rieckpil <philip.riecks@scout24.com>
2019-12-16 07:05:22 +0100

    add Testcontainers to test application

--- spring-boot-hibernate-flyway-best-practices/src/test/java/de/rieckpil/blog/SpringBootHibernateFlywayBestPracticesApplicationTests.java
+++ spring-boot-hibernate-flyway-best-practices/src/test/java/de/rieckpil/blog/SpringBootHibernateFlywayBestPracticesApplicationTests.java
@@ -1,16 +1,41 @@
 package de.rieckpil.blog;
 
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(initializers = SpringBootHibernateFlywayBestPracticesApplicationTests.Initializer.class)
 public class SpringBootHibernateFlywayBestPracticesApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+            .withDatabaseName("postgres")
+            .withPassword("postgres")
+            .withUsername("username");
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    @Test
+    public void contextLoads() {
+    }
 
 }
