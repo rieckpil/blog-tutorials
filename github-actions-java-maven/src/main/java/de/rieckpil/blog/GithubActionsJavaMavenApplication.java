9676fd11839345c9e3c96cbd19f390881bf128c1
rieckpil <philip.riecks@scout24.com>
2019-12-21 11:42:54 +0100

    updates to central workflow

--- github-actions-java-maven/src/main/java/de/rieckpil/blog/GithubActionsJavaMavenApplication.java
+++ github-actions-java-maven/src/main/java/de/rieckpil/blog/GithubActionsJavaMavenApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GithubActionsJavaMavenApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsJavaMavenApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GithubActionsJavaMavenApplication.class, args);
+  }
 }
