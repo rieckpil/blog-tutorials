ac458a1f8eb6006eacea1aa19d5cad1fca70aa4e
rieckpil <philip.riecks@scout24.com>
2020-04-18 22:32:09 +0200

    update example to override only ObjectMapper

--- spring-boot-aws-integration-tests/src/main/java/de/rieckpil/blog/MessagingConfig.java
+++ spring-boot-aws-integration-tests/src/main/java/de/rieckpil/blog/MessagingConfig.java
@@ -3,25 +3,19 @@ package de.rieckpil.blog;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.aws.messaging.config.QueueMessageHandlerFactory;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver;
-
-import java.util.Collections;
 
 @Configuration
 public class MessagingConfig {
 
   @Bean
-  public QueueMessageHandlerFactory queueMessageHandlerFactory(@Autowired ObjectMapper objectMapper) {
-    QueueMessageHandlerFactory factory = new QueueMessageHandlerFactory();
-    MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
-    messageConverter.setObjectMapper(objectMapper);
-    factory.setArgumentResolvers(Collections.singletonList(new PayloadMethodArgumentResolver(messageConverter)));
-    return factory;
+  public MappingJackson2MessageConverter mappingJackson2MessageConverter(@Autowired ObjectMapper objectMapper) {
+    MappingJackson2MessageConverter jackson2MessageConverter = new MappingJackson2MessageConverter();
+    jackson2MessageConverter.setObjectMapper(objectMapper);
+    return jackson2MessageConverter;
   }
 
   @Bean
