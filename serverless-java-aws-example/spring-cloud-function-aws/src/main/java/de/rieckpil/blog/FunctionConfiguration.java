d3cb30fb39d9c5d3225364d7c53c12a4460c8ced
rieckpil <philip.riecks@scout24.com>
2020-05-09 11:03:55 +0200

    add example for Spring Cloud Function

--- serverless-java-aws-example/spring-cloud-function-aws/src/main/java/de/rieckpil/blog/FunctionConfiguration.java
+++ serverless-java-aws-example/spring-cloud-function-aws/src/main/java/de/rieckpil/blog/FunctionConfiguration.java
@@ -1,5 +1,7 @@
 package de.rieckpil.blog;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,18 +11,24 @@ import java.util.function.Function;
 @SpringBootApplication
 public class FunctionConfiguration {
 
+  private static Log logger = LogFactory.getLog(FunctionConfiguration.class);
+
   public static void main(String[] args) {
     SpringApplication.run(FunctionConfiguration.class, args);
   }
 
   @Bean
   public Function<String, String> uppercase() {
-    return value -> value.toUpperCase();
+    return value -> {
+      logger.info("Processing uppercase: " + value);
+      return value.toUpperCase();
+    };
   }
 
-  @Bean
   public Function<String, String> lowercase() {
-    return value -> value.toLowerCase();
+    return value -> {
+      logger.info("Processing lowercase: " + value);
+      return value.toUpperCase();
+    };
   }
-
 }
--- serverless-java-aws-example/spring-cloud-function-aws/src/test/java/de/rieckpil/blog/FunctionConfigurationTests.java
+++ serverless-java-aws-example/spring-cloud-function-aws/src/test/java/de/rieckpil/blog/FunctionConfigurationTests.java
@@ -1,13 +1,37 @@
 package de.rieckpil.blog;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Disabled
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class FunctionConfigurationTests {
 
+  @Autowired
+  private TestRestTemplate rest;
+
   @Test
-  void contextLoads() {
+  public void testUppercase() throws Exception {
+    ResponseEntity<String> result = this.rest.exchange(
+      RequestEntity.post(new URI("/uppercase")).body("hello"), String.class);
+    assertEquals("HELLO", result.getBody());
+  }
+
+  @Test
+  public void testLowercae() throws Exception {
+    ResponseEntity<String> result = this.rest.exchange(
+      RequestEntity.post(new URI("/lowercase")).body("HELLO"), String.class);
+    assertEquals("hello", result.getBody());
   }
 
 }
