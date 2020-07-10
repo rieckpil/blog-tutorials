6c9208602e34058f5e1935be449f941dd7c35798
rieckpil <mail@philipriecks.de>
2020-07-10 11:31:08 +0200

    add dummy tests

--- five-unknown-junit-5-features/src/test/java/de/rieckpil/blog/ParallelTest.java
+++ five-unknown-junit-5-features/src/test/java/de/rieckpil/blog/ParallelTest.java
@@ -1,4 +1,24 @@
 package de.rieckpil.blog;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ParallelTest {
+
+  @Test
+  public void testOne() {
+    assertEquals(4, 2 + 2);
+  }
+
+  @Test
+  public void testTwo() {
+    assertEquals(4, 2 + 2);
+  }
+
+  @Test
+  public void testThree() {
+    assertEquals(4, 2 + 2);
+  }
+
 }
