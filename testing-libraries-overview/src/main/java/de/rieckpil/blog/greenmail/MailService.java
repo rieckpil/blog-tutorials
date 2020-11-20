2850e1224346fb87551879b7079e2a9b956c4a8e
Philip Riecks <mail@philipriecks.de>
2020-11-20 07:05:47 +0100

    add basic GreenMail send and receive example

---
+++ testing-libraries-overview/src/main/java/de/rieckpil/blog/greenmail/MailService.java
@@ -0,0 +1,67 @@
+package de.rieckpil.blog.greenmail;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class MailService {
+
+  private final Session session;
+
+  public MailService(String host, Integer smtpPort, Integer imapPort, String username, String password) {
+    this.session = createSession(host, smtpPort, imapPort, username, password);
+  }
+
+  private Session createSession(String host, Integer smtpPort, Integer imapPort, String username, String password) {
+    Properties props = new Properties();
+
+    props.put("mail.smtp.host", host);
+    props.put("mail.smtp.port", smtpPort);
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+
+    props.put("mail.imap.host", host);
+    props.put("mail.imap.port", imapPort);
+    props.put("mail.imap.auth", "true");
+    props.put("mail.imap.starttls.enable", "true");
+
+    Authenticator authentication = new Authenticator() {
+      @Override
+      protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(username, password);
+      }
+    };
+
+    return Session.getInstance(props, authentication);
+  }
+
+  public void sendToUser(String message, String recipient) throws MessagingException {
+    Message mail = new MimeMessage(session);
+    mail.addHeader("Content-type", "text/plain; charset=UTF-8");
+    mail.setFrom(new InternetAddress("hello@duke.io"));
+    mail.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+    mail.setSubject("A new message for you");
+    mail.setText(message);
+
+    Transport.send(mail);
+  }
+
+  public String retrieveLatestMailForUser(String recipient, String recipientPassword) throws Exception {
+    try (Store store = session.getStore("imap")) {
+      store.connect(recipient, recipientPassword);
+
+      Folder inbox = store.getFolder("INBOX");
+      inbox.open(Folder.READ_ONLY);
+
+      Message[] messages = inbox.getMessages();
+      String latestMessageContent = "NO NEW MESSAGE";
+
+      if (messages.length > 0) {
+        latestMessageContent = messages[0].getContent().toString();
+      }
+
+      return latestMessageContent;
+    }
+  }
+}
---
+++ testing-libraries-overview/src/test/java/de/rieckpil/blog/greenmail/MailServiceTest.java
@@ -0,0 +1,67 @@
+package de.rieckpil.blog.greenmail;
+
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MailServiceTest {
+
+  @RegisterExtension
+  static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP_IMAP);
+
+  private MailService cut;
+
+  @BeforeEach
+  public void setup() {
+
+    greenMail.setUser("admin@java.io", "my_secret");
+
+    this.cut = new MailService(
+      greenMail.getSmtp().getBindTo(),
+      greenMail.getSmtp().getPort(),
+      greenMail.getImap().getPort(),
+      "admin@java.io",
+      "my_secret");
+  }
+
+  @Test
+  void shouldSendMailToRecipient() throws Exception {
+    cut.sendToUser("Hello from Test!", "mike@java.io");
+
+    MimeMessage receivedMessage = greenMail.getReceivedMessages()[0];
+    assertEquals("Hello from Test!", GreenMailUtil.getBody(receivedMessage));
+    assertEquals(1, receivedMessage.getAllRecipients().length);
+    assertEquals("mike@java.io", receivedMessage.getAllRecipients()[0].toString());
+  }
+
+  @Test
+  void shouldReceiveLastEmailForUser() throws Exception {
+
+    Session smtpSession = greenMail.getSmtp().createSession();
+
+    Message msg = new MimeMessage(smtpSession);
+    msg.setFrom(new InternetAddress("test@java.io"));
+    msg.addRecipient(Message.RecipientType.TO,
+      new InternetAddress("mike@java.io"));
+    msg.setSubject("Test");
+    msg.setText("Hello World from GreenMail!");
+    Transport.send(msg);
+
+    greenMail.setUser("mike@java.io", "secret_mike");
+
+    String result = cut.retrieveLatestMailForUser("mike@java.io", "secret_mike");
+
+    assertEquals("Hello World from GreenMail!", result);
+  }
+}
