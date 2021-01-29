09410b0f813a0f73ed552060a06db8f54771cb1d
Philip Riecks <mail@philipriecks.de>
2021-01-29 11:23:12 +0100

    further Mockito examples

--- testing-libraries-overview/src/test/java/de/rieckpil/blog/registration/UserRegistrationServiceTest.java
+++ testing-libraries-overview/src/test/java/de/rieckpil/blog/registration/UserRegistrationServiceTest.java
@@ -1,11 +1,20 @@
 package de.rieckpil.blog.registration;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class UserRegistrationServiceTest {
 
   @Mock
@@ -14,7 +23,31 @@ class UserRegistrationServiceTest {
   @InjectMocks
   private UserRegistrationService cut;
 
+  @Test
+  void shouldNotReCreateExistingUserWrongStubbing() {
+    Mockito.when(userRepository.findByUsername("mike")).thenReturn(new User());
 
+    // Our mock will return null as the stubbing setup doesn't match
+    this.cut.registerUser("duke");
+  }
 
+  @Test
+  void shouldNotReCreateExistingUser() {
+    Mockito.when(userRepository.findByUsername("duke")).thenReturn(new User());
+
+    // Or be more generic
+    Mockito.when(userRepository.findByUsername(ArgumentMatchers.anyString()))
+      .thenReturn(new User());
+
+    User result = this.cut.registerUser("duke");
+  }
+
+  @Test
+  void shouldPropagateException() {
+    Mockito.when(userRepository.findByUsername("devil"))
+      .thenThrow(new RuntimeException("DEVIL'S SQL EXCEPTION"));
+
+    assertThrows(RuntimeException.class, () -> cut.registerUser("devil"));
+  }
 
 }
