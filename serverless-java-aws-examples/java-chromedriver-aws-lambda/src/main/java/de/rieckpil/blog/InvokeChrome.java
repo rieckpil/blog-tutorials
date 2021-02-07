cdee9233923a0eb76c78899e21d06ad22f2c2751
Philip Riecks <mail@philipriecks.de>
2021-02-07 17:09:27 +0100

    add working AWS Lambda container image for Java

--- serverless-java-aws-examples/java-chromedriver-aws-lambda/src/main/java/de/rieckpil/blog/InvokeChrome.java
+++ serverless-java-aws-examples/java-chromedriver-aws-lambda/src/main/java/de/rieckpil/blog/InvokeChrome.java
@@ -2,30 +2,27 @@ package de.rieckpil.blog;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class InvokeChrome implements RequestHandler<String, String> {
+public class InvokeChrome implements RequestHandler<Void, String> {
 
   @Override
-  public String handleRequest(String input, Context context) {
-
-    WebDriverManager.chromedriver()
-      .cachePath("/tmp")
-      .setup();
+  public String handleRequest(Void input, Context context) {
 
     ChromeOptions chromeOptions = new ChromeOptions();
-    chromeOptions.addArguments("--headless", "--single-process", "--disable-dev-shm-usage", "--no-sandbox");
+    chromeOptions.addArguments(
+      "--headless",
+      "--single-process",
+      "--disable-dev-shm-usage",
+      "--no-sandbox");
 
     WebDriver driver = new ChromeDriver(chromeOptions);
 
     driver.get("https://rieckpil.de");
     String pageTitle = driver.getTitle();
-    driver.close();
-
-    System.out.println(pageTitle);
+    driver.quit();
 
     return pageTitle;
   }
