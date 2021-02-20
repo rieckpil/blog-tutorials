5bd1ddba9d9273a19473af1d8974314b176a6cd4
Philip Riecks <mail@philipriecks.de>
2021-02-20 10:39:36 +0100

    add Dispatcher and further recorded request example for MockWebServer

--- testing-libraries-overview/src/test/java/de/rieckpil/blog/mockwebserver/MockWebServerTest.java
+++ testing-libraries-overview/src/test/java/de/rieckpil/blog/mockwebserver/MockWebServerTest.java
@@ -1,6 +1,7 @@
 package de.rieckpil.blog.mockwebserver;
 
 import de.rieckpil.blog.client.JavaHttpClient;
+import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,6 +22,19 @@ public class MockWebServerTest {
 
   private JavaHttpClient cut;
   private MockWebServer mockWebServer;
+
+  private static String DEFAULT_RESPONSE;
+
+  static {
+    try {
+      DEFAULT_RESPONSE = new String(MockWebServerTest.class
+        .getClassLoader()
+        .getResourceAsStream("stubs/random-quote-success.json")
+        .readAllBytes());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
   @BeforeEach
   void setup() throws IOException {
@@ -35,10 +53,7 @@ public class MockWebServerTest {
 
     MockResponse mockResponse = new MockResponse()
       .addHeader("Content-Type", "application/json; charset=utf-8")
-      .setBody(new String(MockWebServerTest.class
-        .getClassLoader()
-        .getResourceAsStream("stubs/random-quote-success.json")
-        .readAllBytes()));
+      .setBody(DEFAULT_RESPONSE);
 
     mockWebServer.enqueue(mockResponse);
 
@@ -64,10 +79,7 @@ public class MockWebServerTest {
   void shouldReturnQuoteOnSlowResponse() throws IOException {
     MockResponse mockResponse = new MockResponse()
       .addHeader("Content-Type", "application/json; charset=utf-8")
-      .setBody(new String(MockWebServerTest.class
-        .getClassLoader()
-        .getResourceAsStream("stubs/random-quote-success.json")
-        .readAllBytes()))
+      .setBody(DEFAULT_RESPONSE)
       .setBodyDelay(2, TimeUnit.SECONDS);
 
     mockWebServer.enqueue(mockResponse);
@@ -82,10 +94,7 @@ public class MockWebServerTest {
 
     MockResponse mockResponse = new MockResponse()
       .addHeader("Content-Type", "application/json; charset=utf-8")
-      .setBody(new String(MockWebServerTest.class
-        .getClassLoader()
-        .getResourceAsStream("stubs/random-quote-success.json")
-        .readAllBytes()));
+      .setBody(DEFAULT_RESPONSE);
 
     mockWebServer.enqueue(mockResponse);
 
@@ -97,5 +106,73 @@ public class MockWebServerTest {
     assertEquals("/mock/qod", recordedRequest.getPath());
     assertEquals("GET", recordedRequest.getMethod());
     assertEquals("application/json", recordedRequest.getHeader("Accept"));
+  }
+
+  @Test
+  void shouldMultipleResponseVerification() throws IOException, InterruptedException {
+
+    MockResponse firstResponse = new MockResponse()
+      .addHeader("Content-Type", "application/json; charset=utf-8")
+      .setBody(DEFAULT_RESPONSE);
+
+    MockResponse secondResponse = new MockResponse()
+      .addHeader("Content-Type", "application/json; charset=utf-8")
+      .setBody(new String(MockWebServerTest.class
+        .getClassLoader()
+        .getResourceAsStream("stubs/random-quote-success.json")
+        .readAllBytes()));
+
+    mockWebServer.enqueue(firstResponse);
+    mockWebServer.enqueue(secondResponse);
+
+    String randomQuote = this.cut.getRandomQuote();
+
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(mockWebServer.url("/mock") + "/foo"))
+      .header("X-Custom-Header", "Duke42")
+      .DELETE()
+      .build();
+
+    HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+    assertEquals(randomQuote, "Vision without action is daydream. Action without vision is nightmare");
+
+    RecordedRequest recordedRequestOne = mockWebServer.takeRequest();
+    assertEquals("/mock/qod", recordedRequestOne.getPath());
+    assertEquals("GET", recordedRequestOne.getMethod());
+    assertEquals("application/json", recordedRequestOne.getHeader("Accept"));
+
+    RecordedRequest recordedRequestTwo = mockWebServer.takeRequest();
+    assertEquals("/mock/foo", recordedRequestTwo.getPath());
+    assertEquals("DELETE", recordedRequestTwo.getMethod());
+    assertEquals("Duke42", recordedRequestTwo.getHeader("X-Custom-Header"));
+  }
+
+  @Test
+  void dispatcherExample() {
+
+    final Dispatcher dispatcher = new Dispatcher() {
+
+      @Override
+      public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
+
+        switch (request.getPath()) {
+          case "/mock/qod":
+            return new MockResponse()
+              .addHeader("Content-Type", "application/json; charset=utf-8")
+              .setBody(DEFAULT_RESPONSE)
+              .setResponseCode(200);
+          case "/mock/foo":
+            return new MockResponse().setResponseCode(500).setBody("SERVER DOWN");
+        }
+        return new MockResponse().setResponseCode(404);
+      }
+    };
+
+    mockWebServer.setDispatcher(dispatcher);
+
+    String randomQuote = this.cut.getRandomQuote();
+
+    assertEquals(randomQuote, "Vision without action is daydream. Action without vision is nightmare");
   }
 }
