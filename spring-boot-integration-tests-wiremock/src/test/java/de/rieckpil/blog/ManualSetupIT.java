f305894739a28dcf0c163653ebd8e93bfe3ab62c
Philip Riecks <mail@philipriecks.de>
2021-04-16 10:07:45 +0200

    prepare WireMock example

--- spring-boot-integration-tests-wiremock/src/test/java/de/rieckpil/blog/ManualSetupIT.java
+++ spring-boot-integration-tests-wiremock/src/test/java/de/rieckpil/blog/ManualSetupIT.java
@@ -3,14 +3,28 @@ package de.rieckpil.blog;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import com.github.tomakehurst.wiremock.verification.LoggedRequest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.exactly;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.*;
@@ -18,7 +32,15 @@ import static org.springframework.boot.test.context.SpringBootTest.*;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ManualSetupIT {
 
+  @Autowired
+  private WebTestClient webTestClient;
+
   private static WireMockServer wireMockServer;
+
+  @DynamicPropertySource
+  static void webClientConfig(DynamicPropertyRegistry registry) {
+    registry.add("todo_base_url", wireMockServer::baseUrl);
+  }
 
   @BeforeAll
   static void startWireMock() {
@@ -42,9 +64,41 @@ class ManualSetupIT {
     assertTrue(wireMockServer.isRunning());
   }
 
+  @Test
+  void verifyRequests() {
+
+    stubForTodosResponse();
+
+    webTestClient
+      .get()
+      .uri("/api/todos")
+      .exchange()
+      .expectStatus().isOk();
+
+    wireMockServer.verify(exactly(1), getRequestedFor(urlEqualTo("/todos"))
+      .withHeader("Accept", equalTo("application/json")));
+
+    List<ServeEvent> allServeEvents = wireMockServer.getAllServeEvents();
+
+    LoggedRequest request = allServeEvents.get(0).getRequest();
+
+    assertEquals("", request.getBodyAsString());
+    assertEquals("http", request.getScheme());
+    assertEquals("GET", request.getMethod().getName());
+
+    List<LoggedRequest> unmatchedRequests = wireMockServer.findAllUnmatchedRequests();
+
+    assertEquals(0, unmatchedRequests.size());
+
+  }
+
   private void stubForTodosResponse() {
+
+    // anyUrl
+    // urlPathMatching
+
     wireMockServer.stubFor(
-      WireMock.get("/todos")
+      WireMock.get(urlEqualTo("/todos"))
         .willReturn(aResponse()
           .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
           .withBody("[]"))
@@ -53,7 +107,7 @@ class ManualSetupIT {
 
   private void stubForFailingTodosResponse() {
     wireMockServer.stubFor(
-      WireMock.get("/todos")
+      WireMock.get(urlEqualTo("/todos"))
         .willReturn(aResponse()
           .withStatus(500)
           .withBody("Server unavailable"))
