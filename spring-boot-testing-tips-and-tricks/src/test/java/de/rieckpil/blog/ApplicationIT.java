f7a68991616a3dc9c3686130db18c38eddd927d6
Philip Riecks <mail@philipriecks.de>
2021-05-12 19:12:58 +0200

    add further example

--- spring-boot-testing-tips-and-tricks/src/test/java/de/rieckpil/blog/ApplicationIT.java
+++ spring-boot-testing-tips-and-tricks/src/test/java/de/rieckpil/blog/ApplicationIT.java
@@ -1,14 +1,24 @@
 package de.rieckpil.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.web.server.LocalManagementPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ApplicationIT {
+
+  @Autowired
+  private WebTestClient webTestClient; // available with Spring WebFlux
+
+  @Autowired
+  private TestRestTemplate testRestTemplate; // available with Spring Web MVC
 
   @LocalServerPort
   private Integer port;
@@ -20,5 +30,21 @@ class ApplicationIT {
   void printPortsInUse() {
     System.out.println(port);
     System.out.println(managementPort);
+  }
+
+  @Test
+  void httpClientExample() {
+
+    // no need for this
+    WebClient webClient = WebClient.builder()
+      .baseUrl("http://localhost:" + port)
+      .build();
+
+    this.webTestClient
+      .get()
+      .uri("/api/customers")
+      .exchange()
+      .expectStatus().is2xxSuccessful();
+
   }
 }
