523eae2de1a325fd0f658394fc7cbb0bc3760c52
Philip Riecks <mail@philipriecks.de>
2021-06-23 07:30:10 +0200

    add course application

---
+++ spring-boot-testing-primer/src/main/java/de/rieckpil/blog/SpringBootTestingPrimerApplication.java
@@ -0,0 +1,13 @@
+package de.rieckpil.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingPrimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingPrimerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-testing-primer/src/test/java/de/rieckpil/blog/SpringBootTestingPrimerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rieckpil.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingPrimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
