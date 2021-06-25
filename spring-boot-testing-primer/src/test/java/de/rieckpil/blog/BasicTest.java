657dccf39ca43f159da3ecdc69c24267bc91caa2
Philip Riecks <mail@philipriecks.de>
2021-06-25 09:05:55 +0200

    enrich primer example

---
+++ spring-boot-testing-primer/src/test/java/de/rieckpil/blog/BasicTest.java
@@ -0,0 +1,13 @@
+package de.rieckpil.blog;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BasicTest {
+
+  @Test
+  void firstTest() {
+    assertEquals("DUKE", "duke".toUpperCase());
+  }
+}
