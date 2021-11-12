264532b94834a48ed1e01686e2e87d36f42cefee
Philip Riecks <mail@philipriecks.de>
2021-11-12 16:43:57 +0100

    use robust version of exposing the host port with Testcontainers

--- spring-boot-selenium-integration-tests/src/test/java/de/rieckpil/blog/IndexControllerIT.java
+++ spring-boot-selenium-integration-tests/src/test/java/de/rieckpil/blog/IndexControllerIT.java
@@ -1,37 +1,42 @@
 package de.rieckpil.blog;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-
+import org.springframework.core.env.Environment;
+import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.BrowserWebDriverContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Testcontainers
 @ExtendWith({ScreenshotOnFailureExtension.class})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IndexControllerIT {
 
+  static BrowserWebDriverContainer<?> container = new BrowserWebDriverContainer<>()
+    .withCapabilities(new ChromeOptions());
+
   @LocalServerPort
   private int port;
 
-  @Container
-  private BrowserWebDriverContainer<?> container = new BrowserWebDriverContainer<>()
-    .withCapabilities(new ChromeOptions());
+  @BeforeAll
+  static void beforeAll(@Autowired Environment environment) {
+    Testcontainers.exposeHostPorts(environment.getProperty("local.server.port", Integer.class));
+    container.start();
+  }
 
   @Test
   void shouldDisplayMessage() {
-    // depending on your operation system try 172.17.0.1 as IP or container.getTestHostIpAddress()
-    this.container.getWebDriver().get("http://host.docker.internal:" + port + "/index");
+    container
+      .getWebDriver()
+      .get(String.format("http://host.testcontainers.internal:%d/index", port));
 
-    WebElement messageElement = this.container.getWebDriver().findElementById("message");
+    WebElement messageElement = container.getWebDriver().findElementById("message");
 
     assertEquals("Integration Test with Selenium", messageElement.getText());
   }
