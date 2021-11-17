76ee4f3516b8a443532af54618bf6d01bc2e3bb7
Philip Riecks <mail@philipriecks.de>
2021-11-17 11:25:47 +0100

    Logger over System.out

--- spring-boot-aws-integration-tests/src/main/java/de/rieckpil/blog/SimpleMessageListener.java
+++ spring-boot-aws-integration-tests/src/main/java/de/rieckpil/blog/SimpleMessageListener.java
@@ -4,12 +4,16 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.messaging.listener.annotation.SqsListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleMessageListener {
+
+  private static final Logger LOG = LoggerFactory.getLogger(SimpleMessageListener.class);
 
   private final AmazonS3 amazonS3;
   private final ObjectMapper objectMapper;
@@ -26,8 +30,10 @@ public class SimpleMessageListener {
 
   @SqsListener(value = "${event-processing.order-event-queue}")
   public void processMessage(@Payload OrderEvent orderEvent) throws JsonProcessingException {
-    System.out.println("Incoming order: " + orderEvent);
+    LOG.info("Incoming order: '{}'", orderEvent);
+
     amazonS3.putObject(orderEventBucket, orderEvent.getId(), objectMapper.writeValueAsString(orderEvent));
-    System.out.println("Successfully uploaded order to S3");
+
+    LOG.info("Successfully uploaded order to S3");
   }
 }
