e73138ee32c60e9a10ae93ac599957bc77f743c3
Branislav Beno <branislav.beno@siemens.com>
2021-11-30 15:08:55 +0100

    Expose the host port with Testcontainers for class BookStoreYouTubeWT

--- write-concise-web-tests-with-selenide/src/test/java/de/rieckpil/blog/BookStoreYouTubeWT.java
+++ write-concise-web-tests-with-selenide/src/test/java/de/rieckpil/blog/BookStoreYouTubeWT.java
@@ -4,18 +4,21 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.junit5.ScreenShooterExtension;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
 import org.testcontainers.containers.BrowserWebDriverContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static com.codeborne.selenide.Selenide.*;
+import static org.testcontainers.Testcontainers.exposeHostPorts;
 
 @Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,7 +27,6 @@ public class BookStoreYouTubeWT {
   @LocalServerPort
   private Integer port;
 
-  @Container
   public static BrowserWebDriverContainer<?> webDriverContainer =
     new BrowserWebDriverContainer<>()
       .withCapabilities(new ChromeOptions()
@@ -35,11 +37,17 @@ public class BookStoreYouTubeWT {
   public static ScreenShooterExtension screenShooterExtension =
     new ScreenShooterExtension().to("target/selenide");
 
+  @BeforeAll
+  static void beforeAll(@Autowired Environment environment) {
+    exposeHostPorts(environment.getProperty("local.server.port", Integer.class));
+    webDriverContainer.start();
+  }
+
   @Test
   public void shouldDisplayBook() {
 
     Configuration.timeout = 2000;
-    Configuration.baseUrl = "http://172.17.0.1:" + port;
+    Configuration.baseUrl = String.format("http://host.testcontainers.internal:%d", port);
 
     RemoteWebDriver remoteWebDriver = webDriverContainer.getWebDriver();
     WebDriverRunner.setWebDriver(remoteWebDriver);
