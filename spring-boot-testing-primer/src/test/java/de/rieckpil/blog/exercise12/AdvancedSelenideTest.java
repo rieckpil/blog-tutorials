b4d7f52a62a91e75c0322f95d62df678004a234b
Philip Riecks <mail@philipriecks.de>
2022-02-21 07:07:09 +0100

    update TSBAP dependencies and Selenide test

--- spring-boot-testing-primer/src/test/java/de/rieckpil/blog/exercise12/AdvancedSelenideTest.java
+++ spring-boot-testing-primer/src/test/java/de/rieckpil/blog/exercise12/AdvancedSelenideTest.java
@@ -3,42 +3,45 @@ package de.rieckpil.blog.exercise12;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
+import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.BrowserWebDriverContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.org.apache.commons.lang.SystemUtils;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.screenshot;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@Testcontainers
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AdvancedSelenideTest {
 
   @LocalServerPort
   private Integer port;
 
-  @Container
   static BrowserWebDriverContainer<?> webDriverContainer =
     new BrowserWebDriverContainer<>()
       .withCapabilities(new ChromeOptions()
         .addArguments("--no-sandbox")
         .addArguments("--disable-dev-shm-usage"));
 
+  @BeforeAll
+  static void beforeAll(@Autowired Environment environment) {
+    Integer port = environment.getProperty("local.server.port", Integer.class);
+    Testcontainers.exposeHostPorts(port);
+    webDriverContainer.start();
+    Configuration.baseUrl = String.format("http://host.testcontainers.internal:%d", port);
+  }
+
   @Test
-  public void shouldDisplayBook() {
-
-    Configuration.baseUrl = SystemUtils.IS_OS_WINDOWS ? "http://host.docker.internal:" : "http://172.17.0.1:";
-    Configuration.baseUrl = Configuration.baseUrl + port;
-
+   void shouldDisplayBook() {
     RemoteWebDriver remoteWebDriver = webDriverContainer.getWebDriver();
     WebDriverRunner.setWebDriver(remoteWebDriver);
 
