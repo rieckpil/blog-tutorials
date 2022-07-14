4c2ef937fe790e67fbbf862696f1c5be0e8a195e
Philip Riecks <mail@philipriecks.de>
2022-07-14 18:26:14 +0200

    update Maven Archetype blueprint article

--- custom-maven-archetype/src/main/resources/archetype-resources/myScript.sh
+++ custom-maven-archetype/src/main/resources/archetype-resources/myScript.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean package && docker build -t ${groupId}/${artifactId} .
+mvn package && docker build -t ${groupId}/${artifactId} .
 docker rm -f ${artifactId} || true && docker run -d -p 9080:9080 -p 9443:9443 --name ${artifactId} ${groupId}/${artifactId}
