5abd5650b7b9a6d48c3b703826d364e8cd40f049
Philip Riecks <mail@philipriecks.de>
2022-07-18 08:42:28 +0200

    update article with top 3 Maven plugins (#95)

--- maven-plugins-to-ensure-quality/src/main/java/de/rieckpil/blog/BadPracticeFileReader.java
+++ maven-plugins-to-ensure-quality/src/main/java/de/rieckpil/blog/BadPracticeFileReader.java
@@ -8,10 +8,10 @@ import java.io.InputStream;
 @Component
 public class BadPracticeFileReader implements CommandLineRunner {
 
-    @Override
-    public void run(String... args) throws Exception {
-        InputStream in = this.getClass().getResourceAsStream("/message.txt");
-        byte[] allBytes = in.readAllBytes();
-        System.out.println(new String(allBytes));
-    }
+  @Override
+  public void run(String... args) throws Exception {
+    InputStream in = this.getClass().getResourceAsStream("/message.txt");
+    byte[] allBytes = in.readAllBytes();
+    System.out.println(new String(allBytes));
+  }
 }
--- maven-plugins-to-ensure-quality/src/test/java/de/rieckpil/blog/ApplicationTests.java
+++ maven-plugins-to-ensure-quality/src/test/java/de/rieckpil/blog/ApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MavenPluginsToEnsureQualityApplicationTests {
+class ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {
+  }
 }
