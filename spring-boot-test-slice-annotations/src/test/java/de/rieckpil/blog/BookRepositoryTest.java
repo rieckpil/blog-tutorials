65dc0456c8dd2c1c880af3652fcb6f53c17597e5
Philip Riecks <mail@philipriecks.de>
2024-01-22 10:27:59 +0100

    add new project

--- spring-boot-test-slice-annotations/src/test/java/de/rieckpil/blog/BookRepositoryTest.java
+++ spring-boot-test-slice-annotations/src/test/java/de/rieckpil/blog/BookRepositoryTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import javax.persistence.EntityManager;
+import jakarta.persistence.EntityManager;
 import javax.sql.DataSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
