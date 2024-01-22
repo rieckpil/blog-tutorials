5fa68d24a6cf566c6cd7271db453318cf5b602a0
Philip Riecks <mail@philipriecks.de>
2024-01-22 14:56:48 +0100

    fix project

--- spring-mockmvc-with-webtestclient/src/test/java/de/rieckpil/blog/DashboardControllerTest.java
+++ spring-mockmvc-with-webtestclient/src/test/java/de/rieckpil/blog/DashboardControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,6 +14,7 @@ import static org.springframework.security.test.web.reactive.server.SecurityMock
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@Import(WebSecurityConfig.class)
 @WebMvcTest(DashboardController.class)
 class DashboardControllerTest {
 
