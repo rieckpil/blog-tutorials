6ba8cb713fb4c47f47e6cf5c551584be37309582
Philip Riecks <mail@philipriecks.de>
2024-01-22 09:58:00 +0100

    introduce parent POM to centrally manage versions

--- buildJdk17Projects.sh
+++ buildJdk17Projects.sh
@@ -5,19 +5,13 @@ set -o pipefail
 
 declare -a arr=(
  "testing-spring-rest-template"
- "spring-boot-datajpatest"
- "spring-boot-aws-integration-tests"
+
  "serverless-java-aws-examples/java-aws-lambda-custom-image"
- "spring-boot-integration-tests-wiremock"
+
  "launchdarkly-java-development-testing-hints"
- "spring-boot-web-mvc-testing"
- "spring-cloud-aws-sqs-testing"
- "spring-websocket-integration-tests"
- "spring-boot-test-mockmvc-webtestclient-testresttemplate"
+
  "random-data-in-java-using-java-faker"
- "spring-boot-feature-toggles-with-togglz"
- "spring-boot-rest-assured"
- "spring-boot-thymeleaf-testing"
+
  "testcontainers-introduction"
  "maven-junit-paralellize-tests"
 )
