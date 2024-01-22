031fdbac2c0eec07e67942e1a8967805ca057e27
Philip Riecks <mail@philipriecks.de>
2024-01-22 16:57:54 +0100

    fix project

--- spring-boot-web-mvc-testing/src/test/java/de/rieckpil/blog/WebhookAuthorizationHandlerInterceptorTest.java
+++ spring-boot-web-mvc-testing/src/test/java/de/rieckpil/blog/WebhookAuthorizationHandlerInterceptorTest.java
@@ -1,7 +1,7 @@
 package de.rieckpil.blog;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- spring-boot-web-mvc-testing/src/test/java/de/rieckpil/blog/WebhookControllerTest.java
+++ spring-boot-web-mvc-testing/src/test/java/de/rieckpil/blog/WebhookControllerTest.java
@@ -3,12 +3,14 @@ package de.rieckpil.blog;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Import(WebSecurityConfig.class)
 @WebMvcTest(value = WebhookController.class, properties = "valid-api-key=test42")
 class WebhookControllerTest {
 
