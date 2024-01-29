170f027f7c54677bbcd42302171fc06d6d106330
Philip Riecks <mail@philipriecks.de>
2024-01-29 10:29:22 +0100

    next fix

--- spring-web-client-oauth2-reactive-stack/src/main/java/de/rieckpil/blog/SecurityConfiguration.java
+++ spring-web-client-oauth2-reactive-stack/src/main/java/de/rieckpil/blog/SecurityConfiguration.java
@@ -2,22 +2,23 @@ package de.rieckpil.blog;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
-    @Bean
-    SecurityWebFilterChain configure(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange(exchanges ->
-                        exchanges.anyExchange().authenticated()
-                )
-                .oauth2Login(withDefaults())
-                .oauth2Client(withDefaults())
-                .build();
-    }
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    http
+      .authorizeRequests()
+      .anyRequest().authenticated()
+      .and()
+      .oauth2Client(Customizer.withDefaults())
+      .oauth2Login(Customizer.withDefaults());
+
+    return http.build();
+  }
+
 }
