eb02858ffbbd36d4004ddbc084f6607215a334f9
Philip Riecks <mail@philipriecks.de>
2024-01-30 09:24:06 +0100

    fix project

--- dynamic-sql-querying-with-pagination/src/main/java/de/rieckpil/tutorials/Person.java
+++ dynamic-sql-querying-with-pagination/src/main/java/de/rieckpil/tutorials/Person.java
@@ -1,11 +1,11 @@
 package de.rieckpil.tutorials;
 
 import com.querydsl.core.annotations.QueryEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.Instant;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import lombok.Data;
 
 @Data
--- spring-web-client-oauth2-reactive-stack/src/main/java/de/rieckpil/blog/SecurityConfiguration.java
+++ spring-web-client-oauth2-reactive-stack/src/main/java/de/rieckpil/blog/SecurityConfiguration.java
@@ -3,18 +3,18 @@ package de.rieckpil.blog;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
 
   @Bean
-  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .anyRequest()
-        .authenticated()
-        .and()
+  public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+    http.authorizeExchange(
+            authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
         .oauth2Client(Customizer.withDefaults())
         .oauth2Login(Customizer.withDefaults());
 
