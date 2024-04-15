8202e8a01db3495c79706de4b466bbf3893d2536
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-04-15 20:44:47 +0530

    adding javadocs for configuration properties

--- azure-blob-storage-azurite-testing/src/main/java/de/rieckpil/configuration/AzureConfigurationProperties.java
+++ azure-blob-storage-azurite-testing/src/main/java/de/rieckpil/configuration/AzureConfigurationProperties.java
@@ -7,6 +7,24 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+/**
+ * Maps configuration values defined in the active {@code .yml} file to the corresponding instance
+ * variables below. The configuration properties are referenced within the application to facilitate
+ * connection with provisioned Azure Blob Storage Container.
+ *
+ * <p><b> Example YAML configuration: </b>
+ *
+ * <pre>
+ * de:
+ *   rieckpil:
+ *     azure:
+ *       blob-storage:
+ *         container-name: "container-name-goes-here"
+ *         connection-string: "connection-string-goes-here"
+ * </pre>
+ *
+ * @see AzureConfiguration
+ */
 @Getter
 @Setter
 @Validated
