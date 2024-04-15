a9778e697e6a1d7b1ebe3e4d5c14ef50948704f2
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-04-15 17:54:48 +0530

    adding blob storage configurations

---
+++ azure-blob-storage-azurite-testing/src/main/java/de/rieckpil/configuration/AzureConfigurationProperties.java
@@ -0,0 +1,33 @@
+package de.rieckpil.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Validated
+@ConfigurationProperties(prefix = "de.rieckpil.azure")
+public class AzureConfigurationProperties {
+
+	@Valid
+	private BlobStorage blobStorage = new BlobStorage();
+
+	@Getter
+	@Setter
+	@Validated
+	public class BlobStorage {
+
+		@NotBlank(message = "Azure Blob Storage container name must be configured")
+		private String containerName;
+
+		@NotBlank(message = "Azure Blob Storage connection string must be configured")
+		private String connectionString;
+
+	}
+
+}
