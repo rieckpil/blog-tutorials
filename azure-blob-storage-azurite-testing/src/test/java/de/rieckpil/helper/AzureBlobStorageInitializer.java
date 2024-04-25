1e39e6ac4872f2b5de37ebe43e5e9ab5ee42d591
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-04-25 12:24:19 +0530

    enhancing AzureBlobStorageInitializer

--- azure-blob-storage-azurite-testing/src/test/java/de/rieckpil/helper/AzureBlobStorageInitializer.java
+++ azure-blob-storage-azurite-testing/src/test/java/de/rieckpil/helper/AzureBlobStorageInitializer.java
@@ -1,35 +1,34 @@
 package de.rieckpil.helper;
 
 import com.azure.storage.blob.BlobServiceClientBuilder;
-import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.atomic.AtomicBoolean;
 import net.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.GenericContainer;
 
-@Slf4j
 public class AzureBlobStorageInitializer implements BeforeAllCallback {
 
-  private static final String AZURITE_IMAGE = "mcr.microsoft.com/azure-storage/azurite:3.29.0";
-  private static final GenericContainer<?> AZURITE_CONTAINER =
+  private static final AtomicBoolean INITIAL_INVOCATION = new AtomicBoolean(Boolean.TRUE);
+
+  private final String AZURITE_IMAGE = "mcr.microsoft.com/azure-storage/azurite:3.29.0";
+  private final GenericContainer<?> AZURITE_CONTAINER =
       new GenericContainer<>(AZURITE_IMAGE)
           .withCommand("azurite-blob", "--blobHost", "0.0.0.0")
           .withExposedPorts(10000);
 
-  private static final String DEFAULT_AZURITE_CONNECTION_STRING =
+  private final String DEFAULT_AZURITE_CONNECTION_STRING =
       "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:%s/devstoreaccount1;";
-  private static final String CONTAINER_NAME = RandomString.make().toLowerCase();
-  private static String CONTAINER_CONNECTION_STRING;
+  private final String CONTAINER_NAME = RandomString.make().toLowerCase();
+  private String CONTAINER_CONNECTION_STRING;
 
   @Override
   public void beforeAll(final ExtensionContext context) {
-    log.info("Creating Azurite container : {}", AZURITE_IMAGE);
-
-    AZURITE_CONTAINER.start();
-    initializeBlobContainer();
-    addConfigurationProperties();
-
-    log.info("Successfully started Azurite container : {}", AZURITE_IMAGE);
+    if (INITIAL_INVOCATION.getAndSet(Boolean.FALSE)) {
+      AZURITE_CONTAINER.start();
+      initializeBlobContainer();
+      addConfigurationProperties();
+    }
   }
 
   private void initializeBlobContainer() {
